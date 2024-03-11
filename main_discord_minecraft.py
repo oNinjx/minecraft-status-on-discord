427b061f1152c5d8579d9f60ed47dfa15f6f0b4d
oNinjx <135127281+oNinjx@users.noreply.github.com>
2024-03-11 20:17:21 +0530

    Update main_discord_minecraft.py

--- main_discord_minecraft.py
+++ main_discord_minecraft.py
@@ -4,15 +4,12 @@ import requests
 import os
 import datetime
 import pytz
-from keep_alive import keep_alive
-keep_alive()
 
-# Retrieve Discord bot token from Replit secrets environment variable
-TOKEN = os.getenv('token')
+TOKEN = 'MTIxNjczMDEyMTI0Mzg1MjkxNA.GJyy-j.WUhj99xRJfrhll5c7egWEWQRIVWIEKaYw3g5W4'
 
 # Minecraft server IP and port
-MINECRAFT_SERVER_IP = 'play.htmc.one'
-MINECRAFT_SERVER_PORT = 25662
+MINECRAFT_SERVER_IP = 'mc.hypixel.net'
+MINECRAFT_SERVER_PORT = 25565
 
 # Initialize Discord client
 intents = discord.Intents.default()
@@ -32,11 +29,11 @@ def get_server_info():
             status = '`💚 ONLINE`'
         else:
             status = '`❤️ OFFLINE`'
-        player_count = data.get('players', {}).get('online', 0) + 4  # Add 4 to the retrieved player count
+        player_count = data.get('players', {}).get('online', 0) + 0  # Add 4 to the retrieved player count
         return status, player_count
     except Exception as e:
         print(f"Error occurred while querying Minecraft server: {e}")
-        return '`❤️ OFFLINE`', 4  # Return default player count as 4 if there's an error
+        return '`❤️ OFFLINE`', 0  # Return default player count as 4 if there's an error
 
 # Function to check if it's 6:38 PM Indian time
 def is_indian_time_638pm():
@@ -52,12 +49,12 @@ async def update_player_count():
 
     # If it's 6:38 PM Indian time, send a message mentioning @here
     if is_indian_time_638pm():
-        channel = client.get_channel(1201058767488430150)  # Replace with your channel ID
+        channel = client.get_channel(1216750826236678145)  # Replace with your channel ID
         await channel.send("@here Server is up and restarted!")
 
     # If player count has changed or server status has changed, update Discord message
     if player_count != prev_player_count or not message_id:
-        channel = client.get_channel(1201058767488430150)  # Replace with your channel ID
+        channel = client.get_channel(1216750826236678145)  # Replace with your channel ID
         if message_id:
             message = await channel.fetch_message(message_id)
             embed = message.embeds[0]
@@ -75,7 +72,7 @@ async def update_player_count():
             embed.add_field(name='> **Versions**', value='```[1.9x-1.20.4x]```          ', inline=False)
             embed.set_image(url='https://cdn.discordapp.com/attachments/1200404861959819274/1209834352515751996/standard.gif?ex=65fad20b&is=65e85d0b&hm=a8a24f66bfa767efe074c455e7650d4a745f4c9bea9fa4056dbc5abd3a6fac88')
             embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/1200404861959819274/1213454042181664848/logo_unede.png?ex=65f58824&is=65e31324&hm=134fb04c959d122d4932de9a9b5dab5db8ef6621563685d5ff4962cf314bc47c')
-            embed.set_footer(text='Made By iNinjaOP')
+            embed.set_footer(text='Made by iNinjaOP for Hometown Network')
             message = await channel.send(embed=embed)
             message_id = message.id
         prev_player_count = player_count
@@ -89,7 +86,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name='Made By iNinjaOP'))
 
     # Delete previous message if it exists
-    channel = client.get_channel(1201058767488430150)  # Replace with your channel ID
+    channel = client.get_channel(1216750826236678145)  # Replace with your channel ID
     async for msg in channel.history(limit=200):
         if msg.author == client.user:
             await msg.delete()
